5e2babff40c6c16cda82b2ebcfbbe2bdd4e935ae
Oltremare9 <1134625169@qq.com>
2017-12-13 03:58:27 +0800

    1213 3：57
    实现fragment的切换 内部代码实现
    缺少 客户服务器连接 数据库建立 筛选 界面美化 登录注册

--- src/main/java/com/example/abccb/project/FragmentA.java
+++ src/main/java/com/example/abccb/project/FragmentA.java
@@ -11,6 +11,12 @@ import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Carson_Ho on 16/7/22.
@@ -23,6 +29,44 @@ public class FragmentA extends Fragment {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragmenta, container, false);
+    }
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        this.getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        Spinner spinner1=(Spinner)getActivity().findViewById(R.id.spinner1);
+        List<String> list=new ArrayList<String>();
+        list.add("浦一");
+        list.add("浦二");
+        list.add("浦三");
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this.getActivity(),android.R.layout.simple_spinner_item,list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter);
+
+        Spinner spinner2=(Spinner)getActivity().findViewById(R.id.spinner2);
+        List<String>list2=new ArrayList<String>();
+        list2.add("一");
+        list2.add("二");
+        list2.add("三");
+        list2.add("四");
+        list2.add("五");
+        ArrayAdapter<String>adapter2=new ArrayAdapter<String>(this.getActivity(),android.R.layout.simple_spinner_item,list2);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+
+
+        Spinner spinner3=(Spinner)getActivity().findViewById(R.id.spinner3);
+        List<String>list3=new ArrayList<String>();
+        list3.add("8-10点");
+        list3.add("10-12点");
+        list3.add("14-16点");
+        list3.add("16-18点");
+        list3.add("晚自习");
+        ArrayAdapter<String>adapter3=new ArrayAdapter<String>(this.getActivity(),android.R.layout.simple_spinner_item,list3);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
     }
 
 
--- src/main/java/com/example/abccb/project/FragmentB.java
+++ src/main/java/com/example/abccb/project/FragmentB.java
@@ -37,14 +37,103 @@ public class FragmentB extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         View view=inflater.inflate(R.layout.fragmentb, container, false);
-
-
-
-
-
-
-
         return view;
+    }
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        temp=0;
+        A1 = (CheckBox) getActivity().findViewById(R.id.A1);
+        A2 = (CheckBox) getActivity().findViewById(R.id.A2);
+        A3 = (CheckBox) getActivity().findViewById(R.id.A3);
+        A4 = (CheckBox) getActivity().findViewById(R.id.A4);
+        B1 = (CheckBox) getActivity().findViewById(R.id.B1);
+        B2 = (CheckBox) getActivity().findViewById(R.id.B2);
+        B3 = (CheckBox) getActivity().findViewById(R.id.B3);
+        B4 = (CheckBox) getActivity().findViewById(R.id.B4);
+        C1 = (CheckBox) getActivity().findViewById(R.id.C1);
+        C2 = (CheckBox) getActivity().findViewById(R.id.C2);
+        C3 = (CheckBox) getActivity().findViewById(R.id.C3);
+        C4 = (CheckBox) getActivity().findViewById(R.id.C4);
+        D1 = (CheckBox) getActivity().findViewById(R.id.D1);
+        D2 = (CheckBox) getActivity().findViewById(R.id.D2);
+        D3 = (CheckBox) getActivity().findViewById(R.id.D3);
+        D4 = (CheckBox) getActivity().findViewById(R.id.D4);
+        seat = (TextView) getActivity().findViewById(R.id.seat);
+        ok = (Button) getActivity().findViewById(R.id.ok);
+        cancle = (Button) getActivity().findViewById(R.id.cancle);
+
+        CheckBox.OnCheckedChangeListener checkboxListener = new CheckBox.OnCheckedChangeListener() {
+
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    if (temp >= 1) {
+                        dialog = new AlertDialog.Builder(getActivity())
+
+                                .setTitle("不太好吧")
+
+                                .setMessage("一人一座，共享良好学习环境\n请先取消选中的座位")
+
+                                .setPositiveButton("确定", null)
+
+                                .show();
+                        buttonView.setChecked(false);
+                        id=buttonView.getId();
+
+                    }else
+                        temp++;
+                } else {
+                    temp--;
+
+                }
+            }
+        };
+        A1.setOnCheckedChangeListener(checkboxListener);
+        A2.setOnCheckedChangeListener(checkboxListener);
+        A3.setOnCheckedChangeListener(checkboxListener);
+        A4.setOnCheckedChangeListener(checkboxListener);
+        B1.setOnCheckedChangeListener(checkboxListener);
+        B2.setOnCheckedChangeListener(checkboxListener);
+        B3.setOnCheckedChangeListener(checkboxListener);
+        B4.setOnCheckedChangeListener(checkboxListener);
+        C1.setOnCheckedChangeListener(checkboxListener);
+        C2.setOnCheckedChangeListener(checkboxListener);
+        C3.setOnCheckedChangeListener(checkboxListener);
+        C4.setOnCheckedChangeListener(checkboxListener);
+        D1.setOnCheckedChangeListener(checkboxListener);
+        D2.setOnCheckedChangeListener(checkboxListener);
+        D3.setOnCheckedChangeListener(checkboxListener);
+        D4.setOnCheckedChangeListener(checkboxListener);
+
+
+        class listen implements View.OnClickListener {
+
+            @Override
+            public void onClick(View v) {
+                if (id>0) {
+                    //发送给服务器端
+                    //判断后接收反馈
+                }
+                ;
+
+            }
+        }
+        class listenCancle implements View.OnClickListener {
+
+            @Override
+            public void onClick(View v) {
+                if (id>0) {
+                    //发送给服务器端
+                    //判断后接收反馈
+                };
+            }
+        }
+        ok.setOnClickListener(new listen());
+        cancle.setOnClickListener(new listenCancle());
+
+
     }
 
 
